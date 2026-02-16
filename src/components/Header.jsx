21a5d74afbbac2446278ab6ce7767908188177da
asanovaelzana7-create <asanovaelzana7@gmail.com>
2026-02-16 17:17:06 +0600

    eee

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
     <div>
-        
+      <div className=" w-full">
+        <a href="">Проекты</a>
+        <a href="">Вакансии</a>
+        <a href="">О нас</a>
+        <a href="">Отзывы</a>
+        <a href="">Контакты</a>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
