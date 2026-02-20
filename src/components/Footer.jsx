f624dd40d034529fa41bda84d0a588e36f622ccd
asanovaelzana7-create <asanovaelzana7@gmail.com>
2026-02-20 17:16:23 +0600

    Header

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer jgykftgfc</div>
+    <div>
+      
+    </div>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,30 @@
 import React from "react";
+import union from "./images/Union (2).svg"
+import flag from "./images/flag.svg"
 
 const Header = () => {
   return (
-    <div>
-      <div className=" w-full">
+    <div id="container">
+
+      <div className="header">
+        <img src={union} alt="union" className="union1"/>
         <a href="">Проекты</a>
         <a href="">Вакансии</a>
         <a href="">О нас</a>
         <a href="">Отзывы</a>
         <a href="">Контакты</a>
+      </div>
+      <div className="hed1">
+        <select name="" id="">
+        <img src={flag} alt="" className="flag"/>
+          <option value="">rus</option>
+          <option value="">eng</option>
+        </select>
+        <div className="hed2">
+          <p>Нужна консультация?</p>
+          <p>0502800202</p>
+        </div>
+        <button className="btn">Написать</button>
       </div>
     </div>
   );
